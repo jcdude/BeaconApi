bc9f1cca1df8d666f5f4fc8cfa4889c14b2e5349
jed.fletcher <jed@gratistech.com>
2019-10-04 13:43:18 -0400

    Add project files.

---
+++ BeaconApi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BeaconApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
