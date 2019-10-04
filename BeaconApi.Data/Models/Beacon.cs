a8996609d2601fdcc418ccd75aabf5c83878c5ed
jed.fletcher <jed@gratistech.com>
2019-10-04 15:35:21 -0400

    Added beacon class and added context

---
+++ BeaconApi.Data/Models/Beacon.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeaconApi.Data.Models
+{
+    public class Beacon
+    {
+        public int id { get; set; } 
+        public string mac { get; set; } 
+    }
+}
